7bc55ac4ad93e3508bc5adbce0e3b940aaf7a607
LeeJunGyun <rainmaker0303@hanmail.net>
2024-07-20 21:16:26 +0900

    no message

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Kotlin
-* [open class](https://github.com/leechoongyon/public-wiki/blob/main/wiki/kotlin/open%20class.md)
+* [open class(=상속)](https://github.com/leechoongyon/public-wiki/blob/main/wiki/kotlin/open%20class.md)
+* [공변성](https://github.com/leechoongyon/public-wiki/blob/main/wiki/kotlin/공변성.md)
--- src/main/kotlin/com/example/CovarianceExample.kt
+++ src/main/kotlin/com/example/CovarianceExample.kt
@@ -1,13 +1,10 @@
 package com.example
 
-
-
 //class Cup<out T>
-//open class Dog      // open 클래스를 선언하면 다른 클래스에서 상속 가능
-//class Puppy: Dog()  // Dog 를 상속받고 있음
-
+//open class Dog      // 상속 가능
+//class Puppy: Dog() // Puppy 가 Dog 를 상속
+//
 //fun main(array: Array<String>) {
 //    val b : Cup<Dog> = Cup<Puppy>() // OK
 //    val a : Cup<Puppy> = Cup<Dog>() // 오류
-//
 //}
